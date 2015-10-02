3464eb962a8041b03f70f51c5680241a411f323b
Rahul Patle <rahulpatle101@gmail.com>
2015-10-02 02:49:55 -0600

    Initian Commit
    The super fast 4 letter app name generator. A quick thing i coded in a
    1 hr. So proud of myself.

---
+++ app.js
@@ -0,0 +1,13 @@
+// var space = false;
+// $(function() {
+//   $(document).keyup(function(evt) {
+//     if (evt.keyCode == 32) {
+//       space = false;
+//     }
+//   }).keydown(function(evt) {
+//     if (evt.keyCode == 32) {
+//       space = true;
+//       console.log("hi");
+//     }
+//   });
+// });
